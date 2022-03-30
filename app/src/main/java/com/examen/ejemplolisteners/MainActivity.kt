d34e50ead4900311c7c9d49594a829978a7f589c
Angel <angloz780@alu.centroafuera.es>
2022-03-30 12:20:28 +0200

    2

--- app/src/main/java/com/examen/ejemplolisteners/MainActivity.kt
+++ app/src/main/java/com/examen/ejemplolisteners/MainActivity.kt
@@ -3,8 +3,12 @@ package com.examen.ejemplolisteners
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var numeroClick = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         //Llamar al botón sin el ViewBinding
         val b = findViewById<Button>(R.id.button)
 
-        
+        //Al hacer click sale un Toast que es un texto
+        b.setOnClickListener{
+            numeroClick++
+            Toast.makeText(this, "Botón ha sido pulsado $numeroClick veces", Toast.LENGTH_LONG).show()
+        }
+        Toast.makeText(this, "Botón ha sido pulsado 0 veces", Toast.LENGTH_LONG).show()
     }
 }
